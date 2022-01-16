c3ad032505755fe81ba387f7186a5d614b52d7b2
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-01-16 06:40:35 +0000

    Add total assistance requests from a student query

---
+++ 4_queries/2_assistance_requests_from_a_student.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(assistance_requests.*) as total_assistances, students.name as name
+FROM assistance_requests
+JOIN students ON student_id = students.id
+WHERE name = 'Elliot Dickinson'
+GROUP BY students.name;
